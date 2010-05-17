8c8f404154c14fda242ed3eccff3ad820dfef823
elias <elias@ubuntu-preto.roteador>
2010-05-17 07:49:33 -0300

    Agora sim. Removi a pasta prj e adicionei os arquivos que antes estavam sendo enviados por e-mail.

--- prj/prj_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ ed2 - Debug ]----------
-	@"mingw32-make.exe"  -j 1 -f "ed2.mk"
-clean:
-	@echo ----------Cleaning project:[ ed2 - Debug ]----------
-	@"mingw32-make.exe"  -j 1 -f "ed2.mk" clean
